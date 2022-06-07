fa1e8655fcc62e91f43b26b07f1a000bd6ea6248
LukeSummerville <luke.l.summerille@gmail.com>
2022-06-07 10:43:18 -0700

    New testament replaced with Proverbs

--- script.js
+++ script.js
@@ -17,43 +17,27 @@ quoteInputElement.addEventListener('input', () => {
       characterSpan.classList.remove('correct')
       characterSpan.classList.remove('incorrect')
       correct = false
-      console.log("is space")
+      //console.log("is space")
     } else if (character === characterSpan.innerText) {
       characterSpan.classList.add('correct')
       characterSpan.classList.remove('incorrect')
     } else {
       characterSpan.classList.remove('correct')
       characterSpan.classList.add('incorrect')
-      //++incorrectCount;
-
-      //document.getElementById("incorrect-count").innerHTML = incorrectCount;
-     // incorrectCount++;
-
       correct = false
     }
   })
 
   if (correct) {
     console.log("Success")
-    //console.log(wordCount)
-    //console.log(arrayQuote.length)
-    //console.log(timerElement.innerText)
     let WPM = (wordCount / timerElement.innerText) * 60
     console.log("Words per second: " + (wordCount / timerElement.innerText))
-    console.log("Words per minute: " + WPM)
     document.getElementById("wpm").innerHTML = ("WPM: " + WPM.toFixed(1));
-
-
-    console.log(arrayQuote.length / startTime)
     renderNewQuote()
   }
 })
 
-// function getRandomQuote() {
-//   return fetch(RANDOM_QUOTE_API_URL)
-//     .then(response => response.json())
-//     .then(data => data.content)
-// }
+
 CHAPTER_LENGTHS = [
     33, 22, 35, 27, 23, 35, 27, 36, 18, 32,
     31, 28, 25, 35, 33, 33, 28, 24, 29, 30,
@@ -61,8 +45,75 @@ CHAPTER_LENGTHS = [
     31
 ]
 
+CHAPTER_LENGTHS1 = [
+  [25,	23,	17,	25,	48,	34,	29,	34,	38,	42,	3,	5,	58,	36,	39,	28,	27,	35,	3,	34,	46,	46,	39,	51,	46,	75,	66,	2],
+  [45,	28,	35,	41,	43,	56,	37,	38,	5,	52,	33,	44,	37,	72,	47,	2],												
+  [8,	52,	38,	44,	39,	49,	5,	56,	62,	42,	54,	59,	35,	35,	32,	31,	37,	43,	48,	47,	38,	71,	56,	53],				
+  [51,	25,	36,	54,	47,	71,	53,	59,	41,	42,	57,	5,	38,	31,	27,	33,	26,	4,	42,	31,	25],							
+  [26,	47,	26,	37,	42,	15,	6,	4,	43,	48,	3,	25,	52,	28,	41,	4,	34,	28,	4,	38,	4,	3, 35,	27,	27,	32,	44,	31],
+  [32,	29,	31,	25,	21,	23,	25,	39,	33,	21,	36,	21,	14,	23,	33,	27],												
+  [31,	16,	23,	21,	13,	2,	4,	13,	27,	33,	34,	31,	13,	4,	58,	24],												
+  [24,	17,	18,	18,	21,	18,	16,	24,	15,	18,	33,	21,	13],															
+  [24,	21,	29,	31,	26,	18],																						
+  [23,	22,	21,	32,	33,	24],																						
+  [3,	3,	21,	23],																								
+  [29,	23,	25,	18],																								
+  [1,	2,	13,	18,	28],																							
+  [12,	17,	18],																									
+  [2,	15,	16,	16,	25,	21],																						
+  [18,	26,	17,	22],																								
+  [16,	15,	15],																									
+  [25],																											
+  [14,	18,	19,	16,	14,	2,	28,	13,	28,	39,	4,	29,	25],															
+  [27,	26,	18,	17,	2],																							
+  [25,	25,	22,	19,	14],																							
+  [21,	22,	18],																									
+  [1,	29,	24,	21,	21],																							
+  [13],																											
+  [15],																											
+  [25],																											
+  [2,	29,	22,	11,	14,	17,	17,	13,	21,	11,	19,	17,	18,	2,	8,	21,	18,	24,	21,	15,	27,	21]		
+]
+
+NEW_TESTAMENT = [
+  'Matthew',
+  'Mark',
+  'Luke',
+  'John',
+  'Acts',
+  'Rom',
+  '1 Cor',
+  '2 Cor',
+  'Gal',
+  'Eph',
+  'Phil',
+  'Col',
+  '1Thess',
+  '2Thess',
+  '1 Tim',
+  '2 Tim',
+  'Titus',
+  'Phlm',
+  'Heb',
+  'James',
+  '1 Peter',
+  '2 Peter',
+  '1 John',
+  '2 John',
+  '3 John',
+  'Jude', 
+  'Rev'
+]
+
+function getRandomBibleVerseNT() {
+  let book = Math.floor(Math.random() * NEW_TESTAMENT.length); // pick random book from 0 to 27
+  let chapter = Math.floor(Math.random() * CHAPTER_LENGTHS1[book].length) //
+  let verse = Math.floor(Math.random() * CHAPTER_LENGTHS1[book][chapter])
+  return NEW_TESTAMENT[book] + " " + chapter + ':' + verse
+}
+
 // Grab random chapter and then grab verse length and randomize?
-function getRandomPassage() {
+function getRandomProverb() {
     let chapter = Math.floor(Math.random() * CHAPTER_LENGTHS.length) 
     //console.log(chapter);
     let verse = Math.floor(Math.random() * CHAPTER_LENGTHS[chapter])
@@ -70,11 +121,11 @@ function getRandomPassage() {
     return 'Proverbs ' + chapter + ':' + verse
 }
 
-function getRandomQuote() {
-    biblePas = getRandomPassage();
+function getBibleVerseFromAPI() {
+    biblePas = getRandomBibleVerseNT();
     //console.log("Before API call")
     //console.log(BIBLE_API_URL)
-    let BIBLE_API_URL = 'https://api.esv.org/v3/passage/text/?q=' + getRandomPassage() + '&include-headings=False&include-footnotes=False&include-verse-numbers=False&include-short-copyright=False&include-passage-references=False&line-length=0&indent-paragraphs=0&indent-poetry=False&indent-paragraphs=0'
+    let BIBLE_API_URL = 'https://api.esv.org/v3/passage/text/?q=' + getRandomBibleVerseNT() + '&include-headings=False&include-footnotes=False&include-verse-numbers=False&include-short-copyright=False&include-passage-references=False&line-length=0&indent-paragraphs=0&indent-poetry=False&indent-paragraphs=0'
     return fetch(BIBLE_API_URL, {
         method: 'GET',
         headers: {
@@ -85,10 +136,7 @@ function getRandomQuote() {
   }
 
   function getTestQuote() {
-    biblePas = getRandomPassage();
-    //console.log("Before API call")
-    //console.log(BIBLE_API_URL)
-    let BIBLE_API_URL = 'https://api.esv.org/v3/passage/text/?q=' + "Proverbs 11:22" + '&include-headings=False&include-footnotes=False&include-verse-numbers=False&include-short-copyright=False&include-passage-references=False&line-length=0&indent-paragraphs=0&indent-poetry=False&indent-paragraphs=0'
+    let BIBLE_API_URL = 'https://api.esv.org/v3/passage/text/?q=' + "John 1:1" + '&include-headings=False&include-footnotes=False&include-verse-numbers=False&include-short-copyright=False&include-passage-references=False&line-length=0&indent-paragraphs=0&indent-poetry=False&indent-paragraphs=0'
     return fetch(BIBLE_API_URL, {
         method: 'GET',
         headers: {
@@ -99,40 +147,30 @@ function getRandomQuote() {
   }
 
 function trimQuote(quote) {
-   // console.log("Before split");
-    //console.log(quote.passages[0]);
     quote = quote.replace(/^\s+|\s+$/gm,'');
     quote = quote.replace(/(\n)/gm, " ");
-   // console.log("Aftr split");
-   // console.log(quote[0]);
     quote = quote.replace(/[“]/g, '"')
     quote = quote.replace(/[”]/g, '"')
     quote = quote.replace(/[’]/g, "'")
-
-  
-    
-    //console.log("Aftr quote");
+    quote = quote.replace(/[—]/g, " -")
     console.log(quote);
     return quote;
 }
+
+let wordCount = 0;  
 function WordCount(str) { 
     return str.split(" ").length;
   }
-  
-  //console.log(WordCount("hello world"));
 
-let wordCount = 0;  
 let timer1 = 0;
 async function renderNewQuote() {
-  const quote = await getRandomQuote()
+  const quote = await getBibleVerseFromAPI()
   //const quote = await getTestQuote()
-  //console.log(WordCount(quote));
 
   document.getElementById("passage").innerHTML = quote.query;
   
   quote[0] = trimQuote(quote.passages[0]);  
   wordCount = WordCount(quote[0]);
-  //console.log(WordCount(quote[0]));
 
   quoteDisplayElement.innerHTML = ''
   quote[0].split('').forEach(character => {
@@ -157,14 +195,5 @@ function getTimerTime() {
   return Math.floor((new Date() - startTime) / 1000)
 }
 
-// var myArray = [
-//     "Apples",
-//     "Bananas",
-//     "Pears"
-//   ];
-  
-//   var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-  
-//   document.body.innerHTML = randomItem;
 console.log("first render")
 renderNewQuote()
